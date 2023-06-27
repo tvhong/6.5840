a39fa48cf6c8691ea900cf636c1442454e982b73
Vy Hong <contact@vyhong.me>
2023-06-26 19:47:00 -0700

    Reduce electionTimeout

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -39,8 +39,8 @@ const (
 // Tester limits 10 heartbeat per second, and expects new leader to be elected
 // within 5s of failure
 const (
-	electionTimeoutMinMs = 1300
-	electionTimeoutMaxMs = 1700
+	electionTimeoutMinMs = 500
+	electionTimeoutMaxMs = 800
 )
 
 // as each Raft peer becomes aware that successive log entries are
